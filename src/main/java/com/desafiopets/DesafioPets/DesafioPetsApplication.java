9f0108fd9904430d9d8f5b0711836cb222ea05d2
CristalyA <crisgavnml@gmail.com>
2023-04-22 20:21:27 -0300

    Creating a new project, Alura challenge

---
+++ src/main/java/com/desafiopets/DesafioPets/DesafioPetsApplication.java
@@ -0,0 +1,13 @@
+package com.desafiopets.DesafioPets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiopets/DesafioPets/DesafioPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiopets.DesafioPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
